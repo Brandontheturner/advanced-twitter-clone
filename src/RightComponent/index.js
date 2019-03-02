862438bda3922b4574826241e0ef7214f3177953
Brandon Turner <brandontheturner@gmail.com>
2019-03-02 15:32:33 -0600

    importing and positioning images in twitter project

--- src/RightComponent/index.js
+++ src/RightComponent/index.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import "./ComponentRight.css";
+import rightTop from "./t-right-top.PNG";
+import rightBottom from "./t-right-bottom.PNG";
 
 class ComponentRight extends Component {
   render() {
     return (
       <div className="right">
-        <div className="right-top" />
-        <div className="right-bottom" />
+        <div className="right-top">
+          <img src={rightTop} alt="Marketing Banner" />
+        </div>
+        <div className="right-bottom">
+          <img src={rightBottom} alt="Info" />
+        </div>
       </div>
     );
   }
